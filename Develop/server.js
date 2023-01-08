e7ef86f7645cd3f8b3b5319c9254abd5d34b3e0f
timo9939 <timothylau20199@gmail.com>
2023-01-08 13:38:25 +0000

    model/index linked

--- Develop/server.js
+++ Develop/server.js
@@ -12,6 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: true }).then((PORT) => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false })
+  .then(function () { // when you are done syncing the db to this file
+    //tell the server app(this file) to start listening
+    app.listen(PORT, function() {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  }
+);
