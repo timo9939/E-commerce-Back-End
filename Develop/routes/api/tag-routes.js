a020bb8821a54417d6937bb53d071bc90404b3c6
timo9939 <timothylau20199@gmail.com>
2023-01-09 20:43:37 +0000

    routes/product-routes finished

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,7 +6,27 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+Tag.findAll({
+
+  include:[
+    {
+      model:Product,
+      attributes:['id','product_name','price','stock','category_id']
+    }
+  ]
+})
+.then(dbTagData=>{
+  if(!dbTagData){
+    res.status(404).json({message:'Tags do not match any result'})
+    return;
+  }
+  res.json(dbTagData)
+})
+.catch(err=>{
+  console.log(err)
+})
 });
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
